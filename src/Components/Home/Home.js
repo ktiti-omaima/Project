7dacddab9c00e54800b149bba7af6168d48a85da
ktiti omaima <ktitiomaima@gmail.com>
2021-06-16 23:14:55 +0200

    page accueil

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,50 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './Home.css';
+import Carousel from 'react-bootstrap/Carousel'
+import img1 from '../../images/image1.jpg'
+import img2 from '../../images/image2.jpg'
+import img3 from '../../images/image3.jpg'
 const Home = () => {
     return (
         <div>
-            <h2 style={{color: "red"}}>Home</h2>
-            <p>
-                Home page....
-            </p>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Meuble interieur</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img2}
+              alt="Second slide"
+            />
+        
+            <Carousel.Caption>
+              <h3>Peugeot 208</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img3}
+              alt="Third slide"
+            />
+        
+            <Carousel.Caption>
+              <h3>Offre d'emploi</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         </div>
     )
 };
